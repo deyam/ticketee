45e1410f692883c1c0d13b5e3e3a88e44d4e657b
Deya Motawie <deya787@gmail.com>
2013-10-21 05:49:41 +0200

    Don't show projects that a user doesn't have permission to see

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,10 @@
 class ProjectsController < ApplicationController
 	before_action :authorize_admin!, except: [:index, :show]
-	before_action :require_signin!, only: [:show]
+	before_action :require_signin!, only: [:index,:show]
 	before_action :set_project, only: [:show,:edit,:update,:destroy]
 	def index
-		@projects = Project.all
+#		@projects = Project.all
+		@projects = Project.for(current_user)
 	end
 	def new
 		@project = Project.new
@@ -52,12 +53,12 @@ class ProjectsController < ApplicationController
 	end
 	private
 	  def set_project
-#	    @project = Project.find(params[:id])
-		@project = if current_user.admin?
-			Project.find(params[:id])
-		else
-	    	@project = Project.viewable_by(current_user).find(params[:id])
-		end
+	    @project = Project.for(current_user).find(params[:id])
+		# @project = if current_user.admin?
+		# 	Project.find(params[:id])
+		# else
+	 #    	@project = Project.viewable_by(current_user).find(params[:id])
+		# end
 	  rescue ActiveRecord::RecordNotFound
 	    flash[:alert] = "The project you were looking" +
 	                    " for could not be found."
--- app/models/project.rb
+++ app/models/project.rb
@@ -4,5 +4,8 @@ class Project < ActiveRecord::Base
 	has_many :permissions, as: :thing
 	scope :viewable_by, ->(user) do
   		joins(:permissions).where(permissions: { action: "view",user_id: user.id} )
-end
+	end
+	scope :for, ->(user) do
+  		user.admin? ? Project.all : Project.viewable_by(user)
+	end
 end
